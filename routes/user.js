a099987391f91c779ad77b290262cd274e7f3980
Shoponfoods Kenya <35798516+shoponfoods@users.noreply.github.com>
2018-03-05 22:17:58 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ router.route('/signup/')
 .get((req, res, next) => {
   res.render('accounts/signup', {
     message: req.flash('errors'),
-    pagetitle: "Signup"
+    pagetitle: "Shopon Foods - Grocery and House Supplies"
   });
 })
 .post((req, res, next) => {
@@ -124,7 +124,7 @@ router.route('/login')
   if (req.user) res.redirect('/');
   res.render('accounts/login', {
     message: req.flash('loginMessage'),
-    pagetitle: "Login"
+    pagetitle: "Shopon Foods - Grocery and House Supplies"
   });
 })
 .post(passport.authenticate('local-login', {
