463855c5664162a785703aa9c1950d145cd18abd
Shoponfoods Kenya <35798516+shoponfoods@users.noreply.github.com>
2018-04-26 23:26:27 +0300

    Delete auth.js

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-exports.isAdmin = function(req, res, next) {
-  if (req.isAuthenticated() && res.locals.user.admin == 1) {
-    next();
-  } else {
-    req.flash('error', 'Please log in as admin');
-    res.redirect('/login');
-  }
-}
