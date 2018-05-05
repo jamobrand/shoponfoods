a3295a349ac3c13892a701ca43e8a210ec1608a2
Shoponfoods Kenya <35798516+shoponfoods@users.noreply.github.com>
2018-05-05 17:53:08 +0300

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -4,15 +4,15 @@ module.exports = {
   secret: "wacha2343k?,",
 
   facebook: {
-    clientID: process.env.FACEBOOK_ID || '172851270015757',
-    clientSecret: process.env.FACEBOOK_SECRET || '98025d0e4a3753e19b0e9db29603d5ed',
+    clientID: process.env.FACEBOOK_ID || '',
+    clientSecret: process.env.FACEBOOK_SECRET || '',
     profileFields: ['id', 'displayName', 'emails'],
     callbackURL: 'http://localhost:8000/auth/facebook/callback'
   },
 
   google: {
-    clientID: process.env.GOOGLE_CLIENT_ID || '159726856122-ffkfrt7ql8chsqfgr573sbejci5nu8jv.apps.googleusercontent.com',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'N3AnNsnffOkxwumzNBAYOR2t',
+    clientID: process.env.GOOGLE_CLIENT_ID || '',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     profileFields: ['id', 'displayName', 'emails'],
     callbackURL: 'http://localhost:8000/auth/google/callback'
   }
